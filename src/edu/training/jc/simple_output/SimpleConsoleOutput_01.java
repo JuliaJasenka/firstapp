f74efde5360b6bb4c96ff90b1db06e82d2eba51a
JuliaJasenka <148490193+JuliaJasenka@users.noreply.github.com>
2023-10-23 21:20:46 +0300

    Add files via upload

---
+++ src/edu/training/jc/simple_output/SimpleConsoleOutput_01.java
@@ -0,0 +1,8 @@
+package edu.training.jc.simple_output;
+
+public class SimpleConsoleOutput_01 {
+	public static void main (String []args) {
+		System.out.println(" A\n B\n C\n D\n E");
+		
+	}
+}
---
+++ src/edu/training/jc/simple_output/SimpleConsoleOutput_02.java
@@ -0,0 +1,8 @@
+package edu.training.jc.simple_output;
+
+public class SimpleConsoleOutput_02 {
+	public static void main (String []arg) {
+		System.out.println("Yesterday all my troubles seemed so far away, now it looks like that they're here to stay.");
+	}
+
+}
