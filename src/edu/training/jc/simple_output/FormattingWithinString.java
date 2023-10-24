e9b5bccbfeb930a246a9132868ea9d7e6b6983d8
JuliaJasenka <148490193+JuliaJasenka@users.noreply.github.com>
2023-10-24 13:39:02 +0300

    Add files via upload

--- src/edu/training/jc/simple_output/FormattingWithinString.java
+++ src/edu/training/jc/simple_output/FormattingWithinString.java
@@ -2,7 +2,9 @@ package edu.training.jc.simple_output;
 
 public class FormattingWithinString {
 	public static void main (String[]args) {
-		System.out.println("1) Todoist - Balancing power and simplicity.\n2) TickTick - Embedded calendars and timers.\n3) Microsoft To Do - Microsoft power users.");
+		System.out.println("1) Todoist - Balancing power and simplicity.");
+		System.out.println("2) TickTick - Embedded calendars and timers.");
+		System.out.println("3) Microsoft To Do - Microsoft power users.");
 	}
 
 }
--- src/edu/training/jc/simple_output/SimpleConsoleOutput_02.java
+++ src/edu/training/jc/simple_output/SimpleConsoleOutput_02.java
@@ -2,7 +2,8 @@ package edu.training.jc.simple_output;
 
 public class SimpleConsoleOutput_02 {
 	public static void main (String []arg) {
-		System.out.println("Yesterday all my troubles seemed so far away, now it looks like that they're here to stay.");
+		System.out.print("Yesterday all my troubles seemed so far away,");
+		System.out.print (" now it looks like that they're here to stay.");
 	}
 
 }
